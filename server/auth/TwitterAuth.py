9e3a943de457280546f9cbf212c797854ad997c0
jg <57115019+joeyagreco@users.noreply.github.com>
2022-05-16 18:29:14 -0500

    added stuff

--- server/auth/TwitterAuth.py
+++ server/auth/TwitterAuth.py
@@ -2,8 +2,8 @@ from server.util.EnvironmentReader import EnvironmentReader
 
 
 class TwitterAuth:
-    API_KEY = EnvironmentReader.get("API_KEY")
-    API_KEY_SECRET = EnvironmentReader.get("API_KEY_SECRET")
-    BEARER_TOKEN = EnvironmentReader.get("BEARER_TOKEN")
-    ACCESS_TOKEN = EnvironmentReader.get("ACCESS_TOKEN")
-    ACCESS_TOKEN_SECRET = EnvironmentReader.get("ACCESS_TOKEN_SECRET")
+    API_KEY = EnvironmentReader.get("TWITTER_API_KEY")
+    API_KEY_SECRET = EnvironmentReader.get("TWITTER_API_KEY_SECRET")
+    BEARER_TOKEN = EnvironmentReader.get("TWITTER_BEARER_TOKEN")
+    ACCESS_TOKEN = EnvironmentReader.get("TWITTER_ACCESS_TOKEN")
+    ACCESS_TOKEN_SECRET = EnvironmentReader.get("TWITTER_ACCESS_TOKEN_SECRET")
