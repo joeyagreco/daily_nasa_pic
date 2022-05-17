ac4278a5c9dde0ea0d3e4a92aab75abe3bac5f45
jg <57115019+joeyagreco@users.noreply.github.com>
2022-05-16 22:22:32 -0500

    added stuff

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
+import os
+
 from server.twitter.TwitterTweeter import TwitterTweeter
 from server.util.ImageDownloader import ImageDownloader
 
 if __name__ == "__main__":
-    ImageDownloader.downloadImageByUrl("https://apod.nasa.gov/apod/image/2205/CoiffeesMW_Barakat_960.jpg", "tmp.jpg", "C:/Users/14143/PycharmProjects/daily_nasa_pic")
-    # tt = TwitterTweeter()
-    # tt.createTweet("test jg", mediaUrls=["img.png"])
+    filePath = "C:/Users/14143/PycharmProjects/daily_nasa_pic"
+    fileName = "tmp.jpg"
+    ImageDownloader.downloadImageByUrl("https://apod.nasa.gov/apod/image/2205/CoiffeesMW_Barakat_960.jpg", fileName, filePath)
+    tt = TwitterTweeter()
+    tt.createTweet("test jg", mediaUrls=[os.path.join(filePath, fileName)])
