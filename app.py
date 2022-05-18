205c995112af8bac1c60ac6b04e05de2694fe7b9
jg <57115019+joeyagreco@users.noreply.github.com>
2022-05-18 12:35:52 -0500

    bug fix

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ from server.util.EnvironmentReader import EnvironmentReader
 
 if __name__ == "__main__":
     LOGGER = CustomLogger.getLogger()
-    HOUR_TO_RUN_BOT_AT = EnvironmentReader.get("HOUR_TO_RUN_BOT_AT")
+    HOUR_TO_RUN_BOT_AT = int(EnvironmentReader.get("HOUR_TO_RUN_BOT_AT"))
     LOGGER.info("STARTING BOT...")
-    LOGGER.info(f"WILL TWEET OUT DAILY AT {DateConverter.getTimeStringFromMilitaryHour(int(HOUR_TO_RUN_BOT_AT))}.")
+    LOGGER.info(f"WILL TWEET OUT DAILY AT {DateConverter.getTimeStringFromMilitaryHour(HOUR_TO_RUN_BOT_AT)}.")
     botRunner = BotRunner()
     botRunner.run(HOUR_TO_RUN_BOT_AT)
